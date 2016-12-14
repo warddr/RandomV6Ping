2bb1e12a9bfbcf926f8f8ecda3fe7f6103f94270
root <root@futureweb02.dco.fusa.be>
2016-12-14 14:22:28 +0100

    update ip adress

--- ping.py
+++ ping.py
@@ -4,5 +4,5 @@ import random
 def gen_hex(counter):
    return ''.join([random.choice('0123456789ABCDEF') for x in range(counter)])
 
-bashCommand = "ping6 2a02:578:8528:111:0:"+ gen_hex(2) + ":" + gen_hex(2) + ":" + gen_hex(2) + " -c1 -i0"
+bashCommand = "ping6 2001:6a8:28c0:2017::"+ gen_hex(2) + ":" + gen_hex(2) + ":" + gen_hex(2) + " -c1 -i0"
 os.system(bashCommand)
