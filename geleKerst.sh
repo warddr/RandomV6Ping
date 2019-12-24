4dcde9b627dbe8ffff3a7e26d9f40529f3f97125
Ward De Ridder <ward@warddr.eu>
2019-12-24 13:47:04 +0100

    Update geleKerst.sh

--- geleKerst.sh
+++ geleKerst.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
 while true; do
- for x in {6..10}; do
-  for y in {0..10}; do
-      ping6 2a05:9b81:2020::FF:FF:00 -c5 -i0 &disown
-  done
- done
+      ping6 2a05:9b81:2020::FF:FF:00 -c1000 -i0 &disown
 done
