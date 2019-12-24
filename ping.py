7d11b4aa06f5ed6cb4d58a1f46b88613dc5b47d0
Ward De Ridder <ward@warddr.eu>
2019-12-24 13:32:40 +0100

    Update ping.py

--- ping.py
+++ ping.py
@@ -4,5 +4,5 @@ import random
 def gen_hex(counter):
    return ''.join([random.choice('0123456789ABCDEF') for x in range(counter)])
 
-bashCommand = "ping6 2001:4c08:2028:2019::"+ gen_hex(2) + ":" + gen_hex(2) + ":" + gen_hex(2) + " -c1 -i0"
+bashCommand = "ping6 2a05:9b81:2020::"+ gen_hex(2) + ":" + gen_hex(2) + ":" + gen_hex(2) + " -c1 -i0"
 os.system(bashCommand)
